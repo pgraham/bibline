92beddf590f36fbc0540e0be023eafcd27374d48
Philip Graham <philip.robert.graham@gmail.com>
2014-09-19 11:15:42 -0400

    Update grunt build to preprocess less files and remove client side less compilation

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,18 @@ module.exports = function (grunt) {
 
 	var targetPath = 'target';
 
+	var copyFilter = (function () {
+
+		var excludePatterns = [
+			'client/www/css/**/*.less'
+		];
+
+		return function (path) {
+			return !grunt.file.isMatch(excludePatterns, path);
+		};
+
+	}());
+
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 
@@ -21,15 +33,42 @@ module.exports = function (grunt) {
 						expand: true,
 						cwd: '<%= clientPath %>/',
 						src: [ '**' ],
-						dest: '<%= buildPath %>'
+						dest: '<%= buildPath %>',
+						filter: copyFilter
 					}
 				]
 			}
 		},
+		less: {
+			client: {
+				options: {
+					ieCompat: false,
+					cleancss: true
+				},
+				files: [
+					{
+						expand: true,
+						cwd: '<%= clientPath %>/www/css/',
+						src: [ '**/*.less' ],
+						dest: '<%= buildPath %>/www/css/',
+						ext: '.css'
+					}
+				]
+			}
+		},
+		processhtml: {
+			client: {
+				files: {
+					'<%= buildPath %>/www/index.html': [ '<%= clientPath %>/www/index.html' ]
+				}
+			}
+		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-processhtml');
 
-	grunt.registerTask('default', [ 'clean', 'copy' ]);
+	grunt.registerTask('default', [ 'clean', 'copy', 'less', 'processhtml' ]);
 };
