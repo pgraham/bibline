dd86c3ea34880ee379036c60e496f64cf42ec8d8
Philip Graham <philip.robert.graham@gmail.com>
2014-09-19 10:20:08 -0400

    Added basic grunt build which copies client source to target directory

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,35 @@
 module.exports = function (grunt) {
 
+	var targetPath = 'target';
+
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json')
+		pkg: grunt.file.readJSON('package.json'),
+
+		targetPath: targetPath,
+		buildPath: targetPath + '/build',
+		workPath: targetPath + '/work',
+		reportsPath: targetPath + '/reports',
+		clientPath: 'client',
+
+		clean: {
+			build: [ "<%= targetPath %>/" ]
+		},
+		copy: {
+			client: {
+				files: [
+					{
+						expand: true,
+						cwd: '<%= clientPath %>/',
+						src: [ '**' ],
+						dest: '<%= buildPath %>'
+					}
+				]
+			}
+		},
 	});
 
-	grunt.registerTask('default', []);
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+
+	grunt.registerTask('default', [ 'clean', 'copy' ]);
 };
