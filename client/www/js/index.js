da9477821724c21c3b7d5a005b90f9ac648187b2
Philip Graham <philip.robert.graham@gmail.com>
2014-09-19 22:03:31 -0400

    Moved event details into a JSON file and created a directive for display session information

--- client/www/js/index.js
+++ client/www/js/index.js
@@ -37,6 +37,5 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        $('.ellipsis').dotdotdot();
     }
 };
