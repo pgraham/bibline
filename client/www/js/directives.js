8160ef4185f94e5915924b1d0a687cb39d178e72
Philip Graham <philip.robert.graham@gmail.com>
2014-09-23 11:39:14 -0400

    Simplified bibSessionList directive by relying on EventController state

--- client/www/js/directives.js
+++ client/www/js/directives.js
@@ -1,20 +1,17 @@
 "use strict";
+var directives = angular.module('biblineDirectives', []);
 
-angular.module('biblineDirectives', [])
-	.directive('bibSessionList', function factory() {
+directives.directive('bibSessionList', function factory() {
+	return {
+		retrict: 'E',
+		templateUrl: 'session-list.html',
+		link: function post(scope, element, attrs) {
+			setTimeout(function () {
+				element.find('.ellipsis').dotdotdot();
+				element.removeClass('out');
+			}, 100);
+		}
+	};
 
-		return {
-			retrict: 'E',
-			scope: {
-				events: '=sessions'
-			},
-			templateUrl: 'session-list.html',
-			link: function post(scope, element, attrs) {
-				setTimeout(function () {
-					element.find('.ellipsis').dotdotdot();
-					element.removeClass('out');
-				}, 100);
-			}
-		};
+});
 
-	});
