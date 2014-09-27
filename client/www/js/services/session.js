4125a9e1be9a476aca5efe771a58de544aa58c35
Philip Graham <philip.robert.graham@gmail.com>
2014-09-26 21:33:03 -0400

    Order loaded sessions by descending start time

--- client/www/js/services/session.js
+++ client/www/js/services/session.js
@@ -5,30 +5,48 @@
 
 var services = angular.module('biblineServices');
 
-services.factory('sessionSrvc', [ '$http', function ($http) {
+services.factory('sessionSrvc', function ($http) {
 
 	var sessions;
 
 	$http.get('data/events.json').success(function (data) {
-		// TODO Sort events by timeslot.startTime in reverse order
-		var indexed = {};
-		angular.forEach(data, function (val, idx) {
-			val.id = idx;
-			indexed[idx] = val;
-		})
-		sessions = indexed;
+		// Assign an ID to each session. The index of the session in the array is
+		// used as the id. Normally this would be specified by a remote data source.
+		for (var i = 0, len = data.length; i < len; i++) {
+			data[i].id = i;
+			data[i].registered = false;
+		}
+
+		// Sort the array by start time in reverse order
+		data.sort(function (a, b) {
+			var aSt = a.timeslot.startTime;
+			var bSt = b.timeslot.startTime;
+
+			if (aSt < bSt) {
+				return 1;
+			} else if (aSt > bSt) {
+				return -1;
+			} else {
+				return 0;
+			}
+		});
+		sessions = data;
 	});
 
 	return {
-		setSessions: function (s) {
-			sessions = s;
+		getSession: function (id) {
+			var sess = null;
+			for (var i = 0, len = sessions.length; i < len; i++) {
+				if (sessions[i].id === id) {
+					sess = sessions[i];
+					break;
+				}
+			}
+			return sess;
 		},
 		getSessions: function () {
 			return sessions;
-		},
-		getSession: function (id) {
-			return sessions[id];
 		}
 	};
 
-} ]);
+});
